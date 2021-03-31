e758226cf66f597d6dc00a7d08c7afae482f9276
Paolo Innocenti <paonath@gmail.com>
2021-03-31 09:47:33 +0200

    done first release

---
+++ src/PH.NlogExtensions/PH.NlogExtensions.Test/LogFileUnitTest.cs
@@ -0,0 +1,92 @@
+using System;
+using System.IO;
+using Xunit;
+
+namespace PH.NlogExtensions.Test
+{
+    public class LogFileUnitTest : UnitTest
+    {
+        [Fact]
+        public void TestEmptyLogger()
+        {
+
+            var file = new FileInfo(@".\logs/log.log");
+            file.Delete();
+
+
+
+            var bytes      = Logger.GetCurrentLogFile("full").GetAwaiter().GetResult();
+            var stringText = Logger.ReadCurrentLogFile("full").GetAwaiter().GetResult();
+            Assert.True(bytes.Length == 0);
+            Assert.True(stringText == null);
+
+        }
+
+        [Fact]
+        public void GetCurrentLogFileOnNullLoggerWillTrhowException()
+        {
+            Logger.Info("A message");
+            Logger = null;
+
+            Exception nullFound = null;
+            try
+            {
+                var bytes = Logger.GetCurrentLogFile("full").GetAwaiter().GetResult();
+            }
+            catch (Exception e)
+            {
+                nullFound = e;
+            }
+
+            Assert.NotNull(nullFound);
+        }
+        
+        [Fact]
+        public void GetCurrentLogFileOnTargetemptyWillTrhowException()
+        {
+            Logger.Info("A message");
+            Exception notFound = null;
+            try
+            {
+                var bytes = Logger.GetCurrentLogFile("").GetAwaiter().GetResult();
+            }
+            catch (Exception e)
+            {
+                notFound = e;
+            }
+
+            Assert.NotNull(notFound);
+        }
+        [Fact]
+        public void GetCurrentLogFileOnTargetNotFoundWillTrhowException()
+        {
+            Logger.Info("A message");
+            Exception notFound = null;
+            try
+            {
+                var bytes = Logger.GetCurrentLogFile("A fake appender target").GetAwaiter().GetResult();
+            }
+            catch (Exception e)
+            {
+                notFound = e;
+            }
+
+            Assert.NotNull(notFound);
+        }
+
+        [Fact]
+        public void GetCurrentLogFile()
+        {
+
+            Logger.Info("A message");
+
+            var bytes      = Logger.GetCurrentLogFile("full").GetAwaiter().GetResult();
+            var stringText = Logger.ReadCurrentLogFile("full").GetAwaiter().GetResult();
+
+
+            Assert.NotEmpty(bytes);
+            Assert.True(!string.IsNullOrEmpty(stringText));
+        }
+
+    }
+}
---
+++ src/PH.NlogExtensions/PH.NlogExtensions/LogfileExtensions.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+using NLog.Targets;
+
+namespace PH.NlogExtensions
+{
+    public static class LogfileExtensions
+    {
+
+        public static async Task<string> ReadCurrentLogFile(this NLog.Logger nlogLogger, string targetFileName)
+        {
+            var bytes = await GetCurrentLogFile(nlogLogger, targetFileName);
+            if (bytes.Length == 0)
+            {
+                return null;
+            }
+
+            return Encoding.UTF8.GetString(bytes, 0, bytes.Length);
+        }
+
+        public static async Task<byte[]> GetCurrentLogFile(this NLog.Logger nlogLogger, string targetFileName)
+        {
+            if (nlogLogger is null)
+            {
+                throw new ArgumentNullException(nameof(nlogLogger));
+            }
+
+            if (string.IsNullOrEmpty(targetFileName) || string.IsNullOrWhiteSpace(targetFileName))
+            {
+                throw new ArgumentException("Value cannot be null or empty.", nameof(targetFileName));
+            }
+
+            var fileTarget = (FileTarget) LogManager.Configuration.FindTargetByName(targetFileName);
+            if (null == fileTarget)
+            {
+                nlogLogger?.Trace("Not found target with name {TargetFileName}: begin throw new ArgumentException",
+                                  targetFileName);
+                throw new ArgumentException($"Not found target with name '{targetFileName}'",
+                                            nameof(targetFileName));
+            }
+
+            var    getInfo  = new LogEventInfo() {TimeStamp = DateTime.UtcNow, Level = NLog.LogLevel.Off};
+            string fileName = fileTarget.FileName.Render(getInfo);
+
+            string path = fileName;
+            if (!System.IO.Path.IsPathRooted(fileName))
+            {
+                var dir = AppContext.BaseDirectory;
+                path = Path.Combine(dir, fileName);
+
+            }
+
+            if (!System.IO.File.Exists(path))
+            {
+                return Array.Empty<byte>();
+            }
+
+
+            using (FileStream stream = File.Open(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                using (var m = new MemoryStream())
+                {
+                    await stream.CopyToAsync(m);
+                    m.Position = 0;
+                    return m.ToArray();
+                }
+
+
+            }
+        }
+
+    }
+}
+
